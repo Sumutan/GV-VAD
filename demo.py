5c3833ef21613f4d0e75b2979cf8eaed2b4f1ccf
cai <1339083025@qq.com>
2025-07-06 20:42:02 +0800

    Clean up project: remove development files and unused code

--- demo.py
+++
@@ -1,8 +0,0 @@
-import os
-name_list = list(open('list/tad-clip-test.list'))
-for i in name_list:
-    i = i[:-1]
-    src_file = i.replace('10crop_clip','sent_emb_n').replace('.npy','_emb.npy')
-    dst_file = src_file.replace('sent_emb_n','test')
-    os.system('cp {} {}'.format(src_file,dst_file))
-    print(i)
